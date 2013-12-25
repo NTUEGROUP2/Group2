76e7ead859e129e1eef1f59c9b7c83665e50ef61
Crazy-Crab <hs01n6060@gmail.com>
2013-12-25 14:11:17 +0800

    undo

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
@@ -20,7 +20,6 @@
 		<li><a href="login.php">會員資料修改</a></li>
 		<li><a href="group.php">管理團隊</a></li>
 		<li><a href="manager_login.php">管理者專區</a></li>
-		<li><a href="reading.php">Symmetric test</a></li>
 	</ul>
 </div>
 <div id="CONTENT" >   
