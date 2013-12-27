a41ce2104aba4f70efa6f3a5544348b987e79ae7
Crazy-Crab <hs01n6060@gmail.com>
2013-12-28 00:06:18 +0800

    Update group.php

--- group.php
+++ group.php
@@ -1,67 +1,67 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>國立臺北教育大學_學習網</title>
-</head>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>小組團隊名單</title>
+	</head>
 
-<body>
+	<body>
 
 
-	<h2>電腦科學學習網</h2>
+        <h2>App Inventor學習網</h2>
 
-<div id="MAIN_NAV" >
+		<div id="MAIN_NAV" >
 
-	<ul>
-        <li><a href="reading.php">有關AppInventor</a></li>  
-        <li><a href="upload.php">App Inventor Classic環境建置</a></li>
-		<li><a href="message.php">留言板</a></li>
-		<li><a href="record.php">歷史紀錄</a></li>
-		<li><a href="login.php">會員資料修改</a></li>
-		<li><b>管理團隊</b></li>
-		<li><a href="manager_login.php">管理者專區</a></li>
-		<li><a href="index.php" style="color:#FF99FF">回首頁</a></li>
-	</ul>
-</div>
-<div id="CONTENT" >   
-	<p>
-		<center>
-		<h2>小組名單<br/></h2>
-		<table border = 1>
-		<tr>
-			<td height = 5 width = 500 align = center> <h3>科系</h3><br/></td>
-			<td height = 5 width = 500 align = center> <h3>姓名</h3><br/></td>
-		</tr>
-		<tr>
-			<td height = 5 width = 500 align = center> <h4>數資系</h4><br/></td>
-			<td height = 5 width = 500 align = center> <h4>田昊民</h4><br/></td>
-		</tr>
-		<tr>
-			<td height = 5 width = 500 align = center> <h4>數資系</h4><br/></td>
-			<td height = 5 width = 500 align = center> <h4>謝長恩</h4><br/></td>
-		</tr>
-		<tr>
-			<td height = 5 width = 500 align = center> <h4>數資系</h4><br/></td>
-			<td height = 5 width = 500 align = center> <h4>黃俊凱</h4><br/></td>
-		</tr>
-		<tr>
-			<td height = 5 width = 500 align = center> <h4>數資系</h4><br/></td>
-			<td height = 5 width = 500 align = center> <h4>盧佳嬅</h4><br/></td>
-		</tr>
-		<tr>
-			<td height = 5 width = 500 align = center> <h4>語創系</h4><br/></td>
-			<td height = 5 width = 500 align = center> <h4>范氏梅</h4><br/></td>
-		</tr>
-		</table>
-		<br/><br/><br/><br/><br/><br/>
-		</center>
-	</p>
-</div>
-<div id="FOOTER">	
-	<p>
-		<br/><br/><br/><br/><br/><br/>
-		<h2><center><br/>Author by <i>Yi-Chan Kao</i> & <i>Gung-Si Chen</i> </center></h2>
-	</p>
-</div>
-</body>
+			<ul>
+				<li><a href="reading.php">有關AppInventor</a></li>  
+				<li><a href="upload.php">App Inventor Classic環境建置</a></li>
+                <li><a href="message.php">留言板</a></li>
+                <li><a href="record.php">歷史紀錄</a></li>
+                <li><a href="login.php">會員資料修改</a></li>
+                <li><b>管理團隊</b></li>
+                <li><a href="manager_login.php">管理者專區</a></li>
+                <li><a href="index.php" style="color:#FF99FF">回首頁</a></li>
+			</ul>
+		</div>
+		<div id="CONTENT" >   
+			<p>
+                <center>
+					<h2>小組名單<br/></h2>
+					<table border = 1>
+						<tr>
+							<td height = 5 width = 500 align = center> <h3>科系</h3></td>
+							<td height = 5 width = 500 align = center> <h3>姓名</h3></td>
+						</tr>
+						<tr>
+							<td height = 5 width = 500 align = center> <h4>數資系</h4></td>
+							<td height = 5 width = 500 align = center> <h4>田昊民</h4></td>
+						</tr>
+						<tr>
+							<td height = 5 width = 500 align = center> <h4>數資系</h4></td>
+							<td height = 5 width = 500 align = center> <h4>謝長恩</h4></td>
+						</tr>
+						<tr>
+							<td height = 5 width = 500 align = center> <h4>數資系</h4></td>
+							<td height = 5 width = 500 align = center> <h4>黃俊凱</h4></td>
+						</tr>
+						<tr>
+							<td height = 5 width = 500 align = center> <h4>數資系</h4></td>
+							<td height = 5 width = 500 align = center> <h4>盧佳嬅</h4></td>
+						</tr>
+						<tr>
+							<td height = 5 width = 500 align = center> <h4>語創系</h4></td>
+							<td height = 5 width = 500 align = center> <h4>范氏梅</h4></td>
+						</tr>
+					</table>
+					<br/><br/><br/><br/><br/><br/>
+                </center>
+			</p>
+		</div>
+		<div id="FOOTER">        
+			<p>
+                <br/><br/><br/><br/><br/><br/>
+                <h2><center><br/>Author by <i>Yi-Chan Kao</i> & <i>Gung-Si Chen</i> </center></h2>
+			</p>
+		</div>
+	</body>
 </html>
