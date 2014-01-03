fb4ef509ab332ed7c53209a7076f4b00bc6990f8
zxc20164 <zxc20164@yahoo.com.tw>
2014-01-03 12:45:26 +0800

    Update message.php

--- message.php
+++ message.php
@@ -20,6 +20,7 @@
 </head>
 
 <body>
+<body background="https://www.google.com.tw/search?q=app+inventor&site=webhp&source=lnms&tbm=isch&sa=X&ei=SDzGUov5DorIkAX4r4GACQ&ved=0CAcQ_AUoAQ&biw=1040&bih=892#facrc=_&imgdii=PQrI4x_sjMhnzM%3A%3BN_1xk6afTfysDM%3BPQrI4x_sjMhnzM%3A&imgrc=PQrI4x_sjMhnzM%3A%3BqpDCwjNshNje5M%3Bhttp%253A%252F%252Fcourses.cs.vt.edu%252F~cs1004%252Fgoogle-app-inventor-icon-300x297.jpg%3Bhttp%253A%252F%252Fcourses.cs.vt.edu%252F~cs1004%252F%3B300%3B297>
 <div id="HEADER">
 	<h2>電腦科學學習網</h2>
 </div>
@@ -42,109 +43,3 @@
 			
       
       $records_per_page = 5;
-
-      
-      if (isset($_GET["page"]))
-        $page = $_GET["page"];
-      else
-        $page = 1;
-
-      
-      $link = create_connection();
-			
-      
-      $sql = "SELECT * FROM message ORDER BY date DESC";	
-      $result = execute_sql("group2", $sql, $link);
-
-      
-      $total_records = mysql_num_rows($result);
-
-      
-      $total_pages = ceil($total_records / $records_per_page);
-
-      
-      $started_record = $records_per_page * ($page - 1);
-
-      
-      mysql_data_seek($result, $started_record);
-
-      
-      $bg[0] = "#D9D9FF";
-      $bg[1] = "#FFCAEE";
-      $bg[2] = "#FFFFCC";
-      $bg[3] = "#B9EEB9";
-      $bg[4] = "#B9E9FF";
-
-      echo "<table width='800' align='center' cellspacing='3'>";
-
-      
-      $j = 1;
-      while ($row = mysql_fetch_assoc($result) and $j <= $records_per_page)
-      {
-        echo "<tr bgcolor='" . $bg[$j - 1] . "'>";
-        echo "<td width='120' align='center'>
-              <img src='messagepicture/" . mt_rand(0, 9) . ".gif'></td>";
-        echo "<td>作者：" . $row["author"] . "<br>";
-        echo "主題：" . $row["subject"] . "<br>";
-        echo "時間：" . $row["date"] . "<hr>";
-        echo $row["content"] . "</td></tr>";
-        $j++;
-      }
-      echo "</table>" ;
-
-      
-      echo "<p align='center'>";
-
-      if ($page > 1)
-        echo "<a href='message.php?page=". ($page - 1) . "'>上一頁</a> ";
-
-      for ($i = 1; $i <= $total_pages; $i++)
-      {
-        if ($i == $page)
-          echo "$i ";
-        else
-          echo "<a href='message.php?page=$i'>$i</a> ";
-      }
-
-      if ($page < $total_pages)
-        echo "<a href='message.php?page=". ($page + 1) . "'>下一頁</a> ";
-      echo "</p>";
-
-      
-      mysql_free_result($result);
-      mysql_close($link);
-    ?>
-    <form name="myForm" method="post" action="post.php">
-      <table border="0" width="800" align="center" cellspacing="0">
-        <tr bgcolor="#0084CA" align="center">
-          <td colspan="2">
-            <font color="#FFFFFF">請在此輸入新的留言</font></td>
-        </tr>
-        <tr bgcolor="#D9F2FF">
-          <td width="15%">作者</td>
-          <td width="85%"><input name="author" type="text" size="50"></td>
-        </tr>
-        <tr bgcolor="#84D7FF">
-          <td width="15%">主題</td>
-          <td width="85%"><input name="subject" type="text" size="50"></td>
-        </tr>
-        <tr bgcolor="#D9F2FF">
-          <td width="15%">內容</td>
-          <td width="85%"><textarea name="content" cols="50" rows="5"></textarea></td>
-        </tr>
-        <tr>
-          <td colspan="2" align="center">
-            <input type="button" value="張貼留言" onClick="check_data()">　
-            <input type="reset" value="重新輸入">
-          </td>
-        </tr>
-      </table>
-    </form>
-</div>
-<div id="FOOTER">	
-	<p>
-		<br/><br/><br/><br/><br/><br/>
-		<h2><center><br/>Author by <i>Yi-Chan Kao</i> & <i>Gung-Si Chen</i> </center></h2>
-	</p>
-</div>
-</body>
