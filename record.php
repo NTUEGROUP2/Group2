8444bc8228a0358576d0f9d9da9f0c17b36290c3
zxc20164 <zxc20164@yahoo.com.tw>
2014-01-03 13:11:55 +0800

    Update record.php

--- record.php
+++ record.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<?php
-include("mysql_connect.php");
-?>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>國立臺北教育大學_學習網</title>
-</head>
-
 <body>
 <div id="HEADER">
 	<h2>電腦科學學習網</h2>
 </div>
 <div id="MAIN_NAV">
+
 	<ul>
 		<li><a href="reading.php">開始閱讀</a></li>
 		<li><a href="upload.php">檔案上傳</a></li>
@@ -26,6 +17,7 @@ include("mysql_connect.php");
 </div>
 <div id="CONTENT">
 	<p>
+	<p align="center"><img src="meterial/reading.jpg"></p>
 		<center>  
 		<h2>閱讀紀錄查詢<br/></h2>
 		<br/>
